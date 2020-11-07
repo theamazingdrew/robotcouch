4c4ae68591c36d6836e981601e82999f9d59b92c
theamazingdrew <dbrown@byu.net>
2020-11-06 19:13:26 -0600

    Update commander.py

--- commander.py
+++ commander.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import rospy
 from geometry_msgs.msg import Twist
-from adafruit_servokit import ServoKit
+from adafruit_servokit import ServoKit #Requires Python3
 
 def callback(data, kit):
    print("hi")
