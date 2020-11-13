593fc9548d0ccf229627bf9bf38d4e863fa0beec
D B <anonymous@gmail.com>
2020-11-12 22:53:24 -0500

    tests

--- couchy_ws/src/joy_control/commander.py
+++ couchy_ws/src/joy_control/commander.py
@@ -10,28 +10,30 @@ def callback(data, kit):
    # TRY USING LINEAR.Z AS THE REVERSE BUTTON. wHEN IT'S -1, GO INTO REVERSE.
    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.linear.x)
    # This is where you put the motor calls and do the math to include angular z
-   kit.continuous_servo[1].throttle = data.linear.x 
-   kit.continuous_servo[0].throttle = data.linear.y
-   
-   
-   #left = data.linear.x
-   #right = data.linear.x
-   ## NEED TO CHECK IF IT'S IN REVERSE. AND FIX THE CALCULATION. if IT'S IN REVERSE, ONLY HAVE NEGATIVE LINEAR.X. OTHERWISE IT HAS TO BE POSITIVE.
-   ## AND IF HAS TO BE BETWEEN 1 AND 0. ADD ERROR CATCHING FOR THAT.
-   ## SOMEWHERE IN HERE YOU NEED TO TEAR DOWN THE ADAFRUIT_SERVOKIT AT CLOSE.
-   #if data.linear.z < 0:
-   #   left = data.linear.x - data.angular.z
-   #else:
-   #   right = data.linear.x + data.angular.z
-   #print(left)
-   #print(right)
-   ##IF THE THROTTLE IS GREATER THAN 1 OR LESS THAN -1, IT NEEDS TO CATCH 
-   ## THE ERROR AND CORRECT. PUT ERROR CATCHING IN HERE BELOW
-   #kit.continuous_servo[1].throttle = round(left, 2) 
-   #kit.continuous_servo[0].throttle = round(right, 2)
-   ## round data.linear.x and angular.z to the nearest tenth?
-   ## then calculate the impact of data.angular.z
-   ## Then update the motor speed
+   left = data.linear.x
+   print(left)
+   right = data.linear.x
+   print(right)
+   # NEED TO CHECK IF IT'S IN REVERSE. AND FIX THE CALCULATION. if IT'S IN REVERSE, ONLY HAVE NEGATIVE LINEAR.X. OTHERWISE IT HAS TO BE POSITIVE.
+   # AND IF HAS TO BE BETWEEN 1 AND 0. ADD ERROR CATCHING FOR THAT.
+   # SOMEWHERE IN HERE YOU NEED TO TEAR DOWN THE ADAFRUIT_SERVOKIT AT CLOSE
+   if data.linear.z < 0:
+      left = data.linear.x - data.angular.z
+   else:
+      right = data.linear.x + data.angular.z
+   print(left)
+   print(right)
+   #kit.continuous_servo[1].throttle = round(data.linear.x, 2) 
+   #kit.continuous_servo[0].throttle = round(data.linear.x, 2)
+   #IF THE THROTTLE IS GREATER THAN 1 OR LESS THAN -1, IT NEEDS TO CATCH 
+   # THE ERROR AND CORRECT. PUT ERROR CATCHING IN HERE BELOW
+   kit.continuous_servo[1].throttle = round(left, 2) 
+   kit.continuous_servo[0].throttle = round(right, 2)
+   # round data.linear.x and angular.z to the nearest tenth?
+   # then calculate the impact of data.angular.z
+   # Then update the motor speed
+   # kit.continuous_servo[1].throttle = xxx 
+   # kit.continuous_servo[0].throttle = xxx 
 
 def move():
     # Initiate the motor controllers
